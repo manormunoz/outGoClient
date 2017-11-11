4d0c400ca3804b97fb4bdf279790205f4d578f59
Orlando Muñoz <orlando.munoz@easytrack.mx>
2017-11-11 11:24:09 -0600

    Correcciones login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -43,7 +43,7 @@ export class LoginPage {
 
   loginGPlus(){
     this.gPlus.login({
-      'webClientId': '1014425556157-l27cn7k2qqq6sut9s9kkv40a5dn42mue.apps.googleusercontent.com',
+      'webClientId': '166122884478-ejrov4r0p9b4grheseh9u9eqha8bbvjp.apps.googleusercontent.com',
       'offline': true
     }).then((res) => {
       this.result = res;
